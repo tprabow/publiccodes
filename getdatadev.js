40565df589a105b9f6b6bbc843c1c0779bba4934
tprabow <30688742+tprabow@users.noreply.github.com>
2021-04-09 13:57:47 +0700

    Add files via upload

---
+++ getdatadev.js
@@ -0,0 +1,16 @@
+
+function getSecret() {
+    return "U2FsdGVkX18ticd6CSVd0mONJoZcCKQi00sxnvQEdaVqpap1MjqAbRJurAc9ewIiMne0LVnCdLGsnU/SpPaHVw==" //Encrypted with AES
+}
+
+function getResource() {
+    return "00000003-0000-0ff1-ce00-000000000000/xapiens.sharepoint.com@ac6d4546-7d1e-48c7-b04e-0cc38318aec8"
+}
+
+function getClientID() {
+    return "a34336a4-497a-4700-9bc8-c7e9ac473d6b@ac6d4546-7d1e-48c7-b04e-0cc38318aec8"
+}
+
+function getKeyword() {
+    return "aaabbb"
+}
