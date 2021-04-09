ca7f8baf64460f2d18eb321d8cc7d1e8c76ef6e2
tprabow <30688742+tprabow@users.noreply.github.com>
2021-04-09 14:02:21 +0700

    Add files via upload

--- getdata.js
+++ getdata.js
@@ -1,6 +1,6 @@
 
 function getSecret() {
-    return "U2FsdGVkX18ticd6CSVd0mONJoZcCKQi00sxnvQEdaVqpap1MjqAbRJurAc9ewIiMne0LVnCdLGsnU/SpPaHVw==" //Encrypted with AES
+    return "U2FsdGVkX1+XPQ+59doyIYzCblUrK/FUnL1CdOdhQj0=" //Encrypted with AES
 }
 
 function getResource() {
@@ -8,7 +8,7 @@ function getResource() {
 }
 
 function getClientID() {
-    return "a34336a4-497a-4700-9bc8-c7e9ac473d6b@ac6d4546-7d1e-48c7-b04e-0cc38318aec8"
+    return "95178e00-e3ef-4812-a843-a6a4dc04535e@ac6d4546-7d1e-48c7-b04e-0cc38318aec8"
 }
 
 function getKeyword() {
